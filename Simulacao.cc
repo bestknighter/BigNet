b649d3c9042e81b0abcc582b0abe30f1fd56af0b
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-11-28 11:47:39 -0200

    Cria alguns nós e conecta em algumas redes.
    Cria nós P2P e nós CSMA (Ethernet), conecta todos os nós Ethernet em suas LANs e conecta os nós de borda das redes LANs Ethernet na rede P2P.

--- Simulacao.cc
+++ Simulacao.cc
@@ -23,12 +23,8 @@ NS_LOG_COMPONENT_DEFINE ("WifiExample");
 
 int main (int argc, char* argv[]) {
 	bool verbose = true;
-	uint32_t nCsma = 3;
-	uint32_t nWifi = 3;
 
 	CommandLine cmd;
-	cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
-	cmd.AddValue ("nWifi", "Number of wifi STA devices", nWifi);
 	cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
 
 	cmd.Parse (argc,argv);
@@ -38,6 +34,40 @@ int main (int argc, char* argv[]) {
 		LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 	}
 
+	NodeContainer p2pNodes;
+	p2pNodes.Create (6); // 6 LANs
+
+	// Como se trata de WAN, vamos usar valores maiores
+	PointToPointHelper pointToPoint;
+	pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("300Mbps"));
+	pointToPoint.SetChannelAttribute ("Delay", StringValue ("500us"));
+
+	NetDeviceContainer p2pDevices;
+	p2pDevices = pointToPoint.Install (p2pNodes);
+
+	// Fazendo todos os nos de LAN Ethernet
+	NodeContainer csmaNodes[4];
+	for (int i = 0; i < 4; i++ ) {
+		// Adicionando nos de borda (1 por rede)
+		csmaNodes[i].Add (p2pNodes.Get (i));
+		// Adicionando nos restantes
+		csmaNodes[i].Create (9);
+	}
+
+	CsmaHelper csma[4];
+	csma[0].SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+	csma[0].SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+	csma[1].SetChannelAttribute ("DataRate", StringValue ("120Mbps"));
+	csma[1].SetChannelAttribute ("Delay", TimeValue (NanoSeconds (2370)));
+	csma[2].SetChannelAttribute ("DataRate", StringValue ("10Mbps"));
+	csma[2].SetChannelAttribute ("Delay", StringValue("1ms"));
+	csma[3].SetChannelAttribute ("DataRate", StringValue ("300Mbps"));
+	csma[3].SetChannelAttribute ("Delay", TimeValue (NanoSeconds (280)));
+
+	NetDeviceContainer csmaDevices[4];
+	for (int i = 0; i < 4; i++ ) {
+		csmaDevices[i] = csma[i].Install (csmaNodes[i]);
+	}
 
 	// TODO: resto do projeto XD
 
